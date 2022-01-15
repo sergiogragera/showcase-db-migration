032e050651ca00cfb427687c03b5a1b6cf5b9510
Sergio Gragera <sergiogragera@gmail.com>
2022-01-15 20:14:04 +0100

    Add precondition to migration

--- db/changelog/V1642268340__add_person_attributes.sql
+++ db/changelog/V1642268340__add_person_attributes.sql
@@ -6,4 +6,6 @@ ALTER TABLE
 ADD
     COLUMN surname VARCHAR(255);
 
+--preconditions onFail:HALT onError:HALT
+--precondition-sql-check expectedResult:1 SELECT COUNT(1) FROM pg_tables WHERE TABLENAME = 'person'
 --rollback ALTER TABLE person DROP COLUMN surname;
